b0e9f723d4ea2179109e8dc46327d54f7fcff050
Dunell <ciemnoki12@gmail.com>
2024-01-22 17:40:10 +0100

    Added validator for pesel on insert and update for USER table

--- plsql/functions.sql
+++ plsql/functions.sql
@@ -1,35 +1,45 @@
-CREATE OR REPLACE FUNCTION birthPesel(pesel IN VARCHAR2) RETURN VARCHAR2 IS
-    year_part  VARCHAR2(20);
-    month_part VARCHAR2(20);
-    day_part   VARCHAR2(20);
-    whatDecade NUMBER;
-    r          NUMBER;
-BEGIN
-    year_part := SUBSTR(pesel, 1, 2);
-    month_part := SUBSTR(pesel, 3, 2);
-    day_part := SUBSTR(pesel, 5, 2);
-    whatDecade := TO_NUMBER(SUBSTR(month_part, 0, 1));
-    r := MOD(TO_NUMBER(SUBSTR(month_part, 0, 1)), 2);
-    IF r = 0 THEN
-        dbms_output.PUT_LINE('');
-    ELSE
-        whatDecade := whatDecade - 1;
-    END IF;
-    whatDecade := CEIL(whatDecade) * 10;
-    month_part := TO_CHAR(TO_NUMBER(month_part) - whatDecade);
+CREATE
+    OR REPLACE PACKAGE VALIDATORS AS
+    FUNCTION birthPesel(p_pesel in VARCHAR2, p_date_of_birth DATE) RETURN BOOLEAN;
+end VALIDATORS;
 
-    IF whatDecade = 80 THEN
-        whatDecade := 18;
-    ELSIF whatDecade = 00 THEN
-        whatDecade := 19;
-    ELSIF whatDecade = 20 THEN
-        whatDecade := 20;
-    ELSIF whatDecade = 40 THEN
-        whatDecade := 21;
-    ELSE
-        whatDecade := 22;
-    END IF;
+CREATE OR REPLACE PACKAGE BODY VALIDATORS AS
+    FUNCTION birthPesel(p_pesel IN VARCHAR2, p_date_of_birth DATE) RETURN BOOLEAN IS
+        v_year_part  VARCHAR2(20);
+        v_month_part VARCHAR2(20);
+        v_day_part   VARCHAR2(20);
+        v_whatDecade NUMBER;
+        v_r          NUMBER;
+        v_calc_date  DATE;
+    BEGIN
+        v_year_part := SUBSTR(p_pesel, 1, 2);
+        v_month_part := SUBSTR(p_pesel, 3, 2);
+        v_day_part := SUBSTR(p_pesel, 5, 2);
+        v_whatDecade := TO_NUMBER(SUBSTR(v_month_part, 0, 1));
+        v_r := MOD(TO_NUMBER(SUBSTR(v_month_part, 0, 1)), 2);
+        IF v_r = 0 THEN
+            dbms_output.PUT_LINE('');
+        ELSE
+            v_whatDecade := v_whatDecade - 1;
+        END IF;
+        v_whatDecade := CEIL(v_whatDecade) * 10;
+        v_month_part := TO_CHAR(TO_NUMBER(v_month_part) - v_whatDecade, 'FM00');
 
-    year_part := TO_CHAR(whatDecade) || year_part;
-    return year_part || '-' || month_part || '-' || day_part;
-end;
+        IF v_whatDecade = 80 THEN
+            v_whatDecade := 18;
+        ELSIF v_whatDecade = 00 THEN
+            v_whatDecade := 19;
+        ELSIF v_whatDecade = 20 THEN
+            v_whatDecade := 20;
+        ELSIF v_whatDecade = 40 THEN
+            v_whatDecade := 21;
+        ELSE
+            v_whatDecade := 22;
+        END IF;
+
+        v_year_part := TO_CHAR(v_whatDecade) || v_year_part;
+        DBMS_OUTPUT.PUT_LINE(v_year_part || '-' || v_month_part || '-' || v_day_part);
+        v_calc_date := TO_DATE(v_year_part || '-' || v_month_part || '-' || v_day_part, 'YYYY-MM-DD');
+        return v_calc_date = p_date_of_birth;
+    end;
+end VALIDATORS;
--- plsql/interface-use.sql
+++ plsql/interface-use.sql
@@ -3,17 +3,19 @@ BEGIN
             p_first_name => 'Kacper',
             p_last_name => 'Kowalski',
             p_role => 'CLIENT',
-            p_email => 'new_kacper_test@example.com'
+            p_email => 'testing_kacper@example.com',
+            p_date_of_birth => TO_DATE('2000-10-07', 'YYYY-MM-DD'),
+            p_pesel => '00250714859'
         );
 END;
 
--- BEGIN
---     USER_INTERFACE.delete_user(p_user_id => 16);
--- END;
---
--- BEGIN
---     USER_INTERFACE.update_user(
---             p_user_id => 19,
---             p_first_name => 'Janek'
---         );
--- END;
+BEGIN
+    USER_INTERFACE.delete_user(p_user_id => 16);
+END;
+
+BEGIN
+    USER_INTERFACE.update_user(
+            p_user_id => 19,
+            p_first_name => 'Janek'
+        );
+END;
